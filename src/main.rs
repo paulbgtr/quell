7237dfa528ceb2a3df2c97aff75ca3a8e02f8743
paulbg <paulbg@hey.com>
2023-11-29 15:24:22 +0200

    feat(main.rs): add sysinfo crate and print process information
    The sysinfo crate is added to the project to provide system information. The main function is modified to create a new System object and iterate over the processes to print their process ID, name, and disk usage. This change allows for monitoring and displaying process information in the application.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+use sysinfo::{ProcessExt, System, SystemExt};
+
 fn main() {
-    println!("Hello, world!");
+    let sys = System::new_all();
+
+    for (pid, process) in sys.processes() {
+        println!("[{}] {} {:?}", pid, process.name(), process.disk_usage());
+    }
 }
